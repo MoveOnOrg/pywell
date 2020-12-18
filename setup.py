8bba4177fe4a39ec139fd603b7c81fe5afd27c07
Sophie Waldman <62553142+sjwmoveon@users.noreply.github.com>
2020-12-18 17:35:29 -0500

    MoveOn copy of pywell

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     author='Scott Reynen',
     packages=['pywell',],
-    url='https://github.com/sreynen/pywell',
+    url='https://github.com/MoveOnOrg/pywell',
     license='MIT',
-    description="A collection of independent Python scripts following the Unix philosphy of doing one thing well.",
+    description="A collection of independent Python scripts following the Unix philosphy of doing one thing well. Forked from https://github.com/sreynen/pywell.",
 )
